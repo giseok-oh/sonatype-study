86d98643cd86c6b62da3951dcf1864c513a7a0e8
giseok-oh <giseok.oh@osckorea.com>
2025-05-23 09:10:27 +0900

    Update: install Nexus Repository

--- provisioning/shell/ssh.sh
+++ provisioning/shell/ssh.sh
@@ -2,7 +2,7 @@
 
 # Setting sudoers
 sudo -i
-sudo echo "vagrant ALL=NOPASSWD: ALL" >> /etc/sudoers
+echo "vagrant ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/vagrant
 
 # Setting SSH
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
@@ -17,7 +17,7 @@ exit
 HOST_PUBLIC_KEY_FILE="$HOME/.ssh/id_rsa.pub"
 VM_USER="vagrant"
 
-TARGET_IPS=("192.168.56.10", "192.168.56.11")
+TARGET_IPS=$1
 
 if [ -f "$HOST_PUBLIC_KEY_FILE" ]; then
     for VM_IP in "${TARGET_IPS[@]}"; do
